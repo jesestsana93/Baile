a9c50c721479e0deb3db53e42651d85834a569b0
chuyudemy <chuyunam93@gmail.com>
2020-09-23 20:03:36 -0500

    pagina terminada

--- js/scritps.js
+++ js/scritps.js
@@ -2,7 +2,6 @@ $(function () {
     /*-----------------------------------------------------------
         1. FUNCIONES PARA EL MENU PRINCIPAL
     -------------------------------------------------------------*/
-
     /*  Inserta y quita la clase ".icono-cerrar" al boton del menú*/
     $('#menu-navegacion .navbar-toggler').click(function () {
         $('.boton-menu').toggleClass('icono-cerrar');
@@ -17,34 +16,102 @@ $(function () {
         $('#menu-navegacion .navbar-collapse').collapse('hide');
     });
 
-
-    //Menu fijo
-    /*var windowHeight = $(window).height(); //altura de la ventana
-    var barraAltura = $('.barra').innerHeight();
-
+    /*-------------------------------------------------------
+       2. OCULTAR Y MOSTRAR BOTON IR ARRIBA
+    ------------------------------------------------------*/
     $(window).scroll(function () {
-        var scroll = $(window).scrollTop(); //nos ayuda a detectar el scrolling!!!
-        if (scroll > windowHeight) {
-            $('.barra').addClass('fixed');
-            $('body').css({
-                'margin-top': barraAltura + 'px'
-            });
-            //console.log("Ya rebasaste la altura de la pantalla");
+        var scrolltop = $(this).scrollTop();
+        if (scrolltop >= 50) {
+            $(".ir-arriba").fadeIn(); //muestra la flechita
         } else {
-            $('.barra').removeClass('fixed');
-            $('body').css({
-                'margin-top': '0px'
-            });
-            //console.log("aun no");
-
+            $(".ir-arriba").fadeOut(); //oculta la flechita
         }
-    });*/
+    });
+
+    /*-------------------------------------------------------
+      3. Iniciando el script Typed.js para las letras de la sección de inicio
+    ------------------------------------------------------*/
+    var typed = new Typed('.typed', { //^2000:indica el tiempo del texto que lo antecede
+        strings: ["en un mismo lugar ^2000"],
+        smartBackspace: false, //para que vuelva a escribir todo incluyendo el mas
+        loop: true, //para que se vuelva a repetir el efecto
+        showCursor: true, //muestra el cursor
+        cursorChar: "|", //puede ir cualquier cosa como un icono de font en el cursor
+        typeSpeed: 50, //velocidad en la que aparecen los caracteres
+        backSpeed: 30, //velocidad con la que se borran los caracteres
+        startDelay: 800 //tiempo en el que se inicia la animacion
+        /*shuffle: true //los elementos se muestran de manera aleatoria*/
+    });
 
     /*-----------------------------------------------------------
-    3. INICIANDO "page-scroll-to-id"
+       4. INICIANDO VENOBOX
+    -------------------------------------------------------------*/
+    $('.venobox-video').venobox({
+        autoplay: true,
+        bgcolor: 'rgba(255, 255, 255, 0.4)',
+        border: '5px',
+        closeColor: '#fff',
+        overlayColor: 'rgba(12, 60, 22, 0.83)',
+        spinner: 'three-bounce'
+    });
+
+    /*-----------------------------------------------------------
+    2. CONFIGURACION DE SLICK PARA LA PARTE DE ALGUNOS DE NUESTROS CLIENTES
+    ------------------------------------------------------------*/
+    $('.sociales').slick({
+        dots: false,
+        infinite: true,
+        speed: 800,
+        arrows: false,
+        slidesToShow: 8,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 500,
+        responsive: [{
+                //Vista lg >=992px
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 5,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false
+                }
+            },
+            {
+                //Vista md >=768px
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false
+                }
+            },
+            {
+                //Vista sm >=576px
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false
+                }
+            }
+        ]
+    });
+
+
+    /*-----------------------------------------------------------
+    6. INICIANDO "page-scroll-to-id"
     -------------------------------------------------------------*/
     $('#menu-principal a').mPageScroll2id({
         offset: 100,
         highlightClass: 'active'
     });
+
+    /*------------------------------------------------------------
+    7. INICIANDO WOW.js para las animaciones en la página
+    --------------------------------------------------------------*/
+    new WOW().init();
+
 });
